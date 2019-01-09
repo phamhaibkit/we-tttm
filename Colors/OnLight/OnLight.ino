d7e26022fedf440997e8079d95ab3fa51d2833cb
tma_pvhai <pvhai@tma.com.vn>
2019-01-09 14:39:26 +0700

    Edit OnLight

--- Colors/OnLight/OnLight.ino
+++ Colors/OnLight/OnLight.ino
@@ -1,25 +1,28 @@
 int buttonPin = 2;    // select the input pin for the potentiometer
 int lightPin = 5;      // select the pin for the LED
+bool flagDone = false;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
   pinMode(lightPin, OUTPUT);
 
-  digitalWrite(buttonPin, LOW);
   digitalWrite(lightPin, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int buttonValue = digitalRead(buttonPin);
-  if (buttonValue) {
-    digitalWrite(lightPin, HIGH);
-    delay(11000);
-    digitalWrite(lightPin, LOW);
-    delay(1000);
-    while (1) {
+  if (!flagDone) {
+    int buttonValue = digitalRead(buttonPin);
+    Serial.println(buttonValue);
+    if (buttonValue) {
+      digitalWrite(lightPin, HIGH);
+      delay(11000);
+      digitalWrite(lightPin, LOW);
+      delay(1000);
+      flagDone = true;
     }
   }
+  Serial.println("AAAAA");
+  delay(1000);
 }
