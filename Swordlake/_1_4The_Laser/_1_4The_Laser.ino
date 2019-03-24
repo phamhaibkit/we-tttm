ac13a961d624c67445cc37115a1f5783b4e830bd
pvhai <phamhaibkit@gmail.com>
2019-03-24 11:54:22 +0700

    SW2

--- Swordlake/_1_4The_Laser/_1_4The_Laser.ino
+++ Swordlake/_1_4The_Laser/_1_4The_Laser.ino
@@ -1,6 +1,6 @@
 
 int tagPin = 2;    // select the input pin for the potentiometer
-int doorPin = 12;
+int laserPin = 3;
 
 bool flagDone = false;
 
@@ -8,20 +8,21 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(tagPin, INPUT_PULLUP);
-  pinMode(doorPin, OUTPUT);
-  digitalWrite(doorPin, LOW);
+  pinMode(laserPin, OUTPUT);
+  digitalWrite(laserPin, LOW);
 }
 
 void loop() {
   if (!flagDone) {
     int buttonValue = digitalRead(tagPin);
     Serial.println(buttonValue);
-    if (buttonValue) {
+    if (!buttonValue) {
       Serial.println("Dung CMNR CMNR CMNR");
-      digitalWrite(doorPin, HIGH);
+      digitalWrite(laserPin, HIGH);
       delay(1000);
       flagDone = true;
     }
   }
   delay(100);
 }
+
