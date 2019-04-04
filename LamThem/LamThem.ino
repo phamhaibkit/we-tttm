253b2a8e105448cdc89a5a38c1eecb060d01f2fb
pvhai <phamhaibkit@gmail.com>
2019-04-04 10:59:50 +0700

    SL laser,lam them

---
+++ LamThem/LamThem.ino
@@ -0,0 +1,63 @@
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+SoftwareSerial mySoftwareSerial(5, 6); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+
+const int trig = 3;     // chân trig của HC-SR04
+const int echo = 2;     // chân echo của HC-SR04
+
+int count = 0;
+
+void setup() {
+  Serial.begin(9600);
+  mySoftwareSerial.begin(9600);
+  pinMode(trig, OUTPUT);  // chân trig sẽ phát tín hiệu
+  pinMode(echo, INPUT);   // chân echo sẽ nhận tín hiệu
+
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    //while(true);
+  }
+  delay(2000);
+  Serial.println(F("DFPlayer Mini online."));
+
+  myDFPlayer.volume(30);  //Set volume value. From 0 to 30
+}
+
+void loop() {
+  unsigned long duration; // biến đo thời gian
+  int distance;           // biến lưu khoảng cách
+
+  /* Phát xung từ chân trig */
+  digitalWrite(trig, 0);  // tắt chân trig
+  delayMicroseconds(2);
+  digitalWrite(trig, 1);  // phát xung từ chân trig
+  delayMicroseconds(5);   // xung có độ dài 5 microSeconds
+  digitalWrite(trig, 0);  // tắt chân trig
+
+  /* Tính toán thời gian */
+  // Đo độ rộng xung HIGH ở chân echo.
+  duration = pulseIn(echo, HIGH);
+  // Tính khoảng cách đến vật.
+  distance = int(duration / 2 / 29.412);
+
+  /* In kết quả ra Serial Monitor */
+  Serial.print(distance);
+  Serial.println("cm");
+  if (distance < 50) {
+    count++;
+  }
+  if (count >= 2) {
+    myDFPlayer.play(1);
+    delay(1000);
+    while (1) {
+    }
+  }
+  delay(500);
+  //    myDFPlayer.play(1);
+}
--- Swordlake/_1_4The_Laser/_1_4The_Laser.ino
+++ Swordlake/_1_4The_Laser/_1_4The_Laser.ino
@@ -1,28 +1,127 @@
+/*
+  Chương trình đọc RFID từ RFID-RC522
+  Cắm chân (Arduino Uno):
+  MOSI: Pin 11
+  MISO: Pin 12
+  SCK: Pin 13
+  SDA: Pin 10
+  RST: Pin 9
+*/
 
-int tagPin = 2;    // select the input pin for the potentiometer
-int laserPin = 3;
 
-bool flagDone = false;
+#include <SPI.h>
+#include "RFID.h"
 
-void setup() {
-  // put your setup code here, to run once:
+#define SS_PIN 10
+#define RST_PIN 9
+
+#define SS_PIN_1 8
+#define RST_PIN_1 7
+
+RFID rfid(SS_PIN, RST_PIN);
+RFID rfid_1(SS_PIN_1, RST_PIN_1);
+
+int doorPin = 3;
+int namchamPin = 2;
+
+int tag[5] = {182, 209, 193, 249, 95};
+int tag_1[5] = {150, 249, 183, 249, 33};
+
+int tagB[5] = {230, 186, 191, 249, 26};
+int tagB_1[5] = {54, 39, 193, 249, 41};
+
+int newtag[5] = {0, 0, 0, 0, 0};
+int newtag_1[5] = {0, 0, 0, 0, 0};
+
+int ok = -1;
+
+void setup()
+{
   Serial.begin(9600);
-  pinMode(tagPin, INPUT_PULLUP);
-  pinMode(laserPin, OUTPUT);
-  digitalWrite(laserPin, LOW);
+  SPI.begin();
+
+  rfid.init();
+  rfid_1.init();
+
+  pinMode(namchamPin, INPUT_PULLUP);
+  pinMode(doorPin, OUTPUT);
+  digitalWrite(doorPin, LOW);
 }
 
-void loop() {
-  if (!flagDone) {
-    int buttonValue = digitalRead(tagPin);
-    Serial.println(buttonValue);
-    if (!buttonValue) {
-      Serial.println("Dung CMNR CMNR CMNR");
-      digitalWrite(laserPin, HIGH);
-      delay(1000);
-      flagDone = true;
+void loop()
+{
+  readTags();
+  delay(200);
+}
+
+
+void readTags()
+{
+  ok = -1;
+
+  if (rfid.isCard()) {
+    if (rfid.readCardSerial()) {
+      for (int i = 0; i < 5; i++) {
+        newtag[i] = rfid.serNum[i];
+      }
+    }
+    Serial.println("1 st Card");
+    for (int i = 0; i < 5; i++) {
+      Serial.print(newtag[i]);
+      Serial.print(",");
+    }
+    delay(100);
+    if (compareTag(newtag, tag) || compareTag(newtag, tagB)) {
+      if (rfid_1.isCard()) {
+        if (rfid_1.readCardSerial()) {
+          for (int i = 0; i < 5; i++) {
+            newtag_1[i] = rfid_1.serNum[i];
+          }
+        }
+        Serial.println("2 nd Card");
+        for (int i = 0; i < 5; i++) {
+          Serial.print(newtag_1[i]);
+          Serial.print(",");
+        }
+        delay(100);
+        if (compareTag(newtag_1, tag_1) || compareTag(newtag_1, tagB_1)) {
+          Serial.println("AKKKKKKKKKKKKKKAAAKKK");
+          int valueNamcham = digitalRead(namchamPin);
+          Serial.println(valueNamcham);
+          if (!valueNamcham) {
+            Serial.println("KICHKICHKICHKICHKICHKICHKICH");
+            digitalWrite(doorPin, HIGH);
+            delay(1000);
+            while (1) {
+              //                  Serial.println("LOOOPLOOPLOOOP---------");
+            }
+          }
+        }
+
+        rfid.halt();
+      }
+    }
+
+    rfid.halt();
+  }
+
+}
+
+// Compare tag
+boolean compareTag(int aa[5], int bb[5])
+{
+  boolean ff = false;
+  int fg = 0;
+  for (int cc = 0 ; cc < 5 ; cc++)
+  {
+    if (aa[cc] == bb[cc])
+    {
+      fg++;
     }
   }
-  delay(100);
+  if (fg == 5)
+  {
+    ff = true;
+  }
+  return ff;
 }
-
