e8d4bf9c9c814aa832ebc2b9e8eab1a0df5a2e4a
pvhai <phamhaibkit@gmail.com>
2019-04-12 12:06:58 +0700

    Cau dao

--- Hunger Game/CauDao/CauDao.ino
+++ Hunger Game/CauDao/CauDao.ino
@@ -1,25 +1,45 @@
 
-const int buttonPin[6] = {3, 4, 5, 6, 7, 8};
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+SoftwareSerial mySoftwareSerial(5, 6); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+
+const int buttonPin[6] = {3, 8, 9, 10, 11};
 const int doorPin = 12;
 
-int pinCount = 6;
+int pinCount = 5;
 
 // Các biến này có thể thay đổi giá trị được
 int buttonPushCounter = 0;   // số button được nhấn
 String trueString = "";
 
-int buttonState[6] = {0, 0, 0, 0, 0, 0};
-int trueState[6] = {0, 0, 0, 0, 1, 1};
+int buttonState[6] = {0, 0, 0, 0, 0};
+int trueState[6] = {1, 0, 1, 1, 1};
 
 bool flagLoop = false;
 
 void setup() {
   Serial.begin(9600); //Bật cổng Serial ở baudrate 9600
+  mySoftwareSerial.begin(9600);
   for (int thisPin = 0; thisPin < pinCount; thisPin++) {
     pinMode(buttonPin[thisPin], INPUT_PULLUP);
   }
   pinMode(doorPin, OUTPUT); // Cài đặt đèn DOOR là OUTPUT
   digitalWrite(doorPin, LOW);
+
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    //while(true);
+  }
+  delay(2000);
+  Serial.println(F("DFPlayer Mini online."));
+
+  myDFPlayer.volume(30);  //Set volume value. From 0 to 30
 }
 
 void loop() {
@@ -35,6 +55,7 @@ void loop() {
     if (compareArr(buttonState, trueState)) {
       Serial.println("Dung CMNR CMNR CMNR");
       digitalWrite(doorPin, HIGH);
+      myDFPlayer.play(1);
       delay(1000);
       flagLoop = true;
     }
